69416541b4b5c124066b5b9e01315adf9a795005
rwik19 <rwik@LAPTOP-AAM1J8AM.localdomain>
2020-03-30 09:53:26 +0530

    Add file

---
+++ reverse_int.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int reverse(int);
+
+int main(void)
+{
+    int number;
+    printf("Enter a natural number: ");
+    scanf("%d", &number);
+    printf("Reverse of %d is %d\n", number, reverse(number));
+}
+
+int reverse(int n)
+{
+    int reverse = 0;
+    while(n)
+    {
+        reverse *= 10;
+        reverse += n % 10;
+        n /= 10;
+    }
+    return reverse;
+}
