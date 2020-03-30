c051bde48274607eaaa0d3e8c3575a7ee17e08b2
rwik19 <rwik@LAPTOP-AAM1J8AM.localdomain>
2020-03-30 20:11:54 +0530

    Add changes

---
+++ essay.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+int isvowel(char);
+int isletter(char);
+int isBlank(char);
+
+int main(void)
+{
+    int words = 0, vowels = 0, letters = 0, blanks = 0, d = 1;
+    char c;
+    FILE *file = fopen("essay.txt", "r");
+    if(file == NULL)
+    {
+        printf("File does not exist.\n");
+        return 1;
+    }
+    while((c = fgetc(file)) != EOF)
+    {
+        if(isBlank(c))
+        {
+            blanks++;
+            d = 1;
+        }
+        else if(isletter(c))
+        {
+            letters++;
+            if(isvowel(c))
+            {
+                vowels++;
+            }
+            if(d)
+            {
+                words++;
+            }
+            d = 0;
+        }
+        else if(c=='-')
+        {
+            d = 0;
+        }
+        else
+        {
+            d = 1;
+        }
+    }
+    fclose(file);
+    printf("Words = %d\nLetters = %d\nVowels = %d\nBlank Spaces = %d\n",words,letters,vowels,blanks);
+}
+
+int isvowel(char c)
+{
+    c = tolower(c);
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isletter(char c)
+{
+    c = tolower(c);
+    if(c>='a' && c<='z')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isBlank(char c)
+{
+    if(c==' ')
+    {
+        return 1;
+    }
+    return 0;
+}
