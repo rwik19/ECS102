dd76a5dd93d4434e49d9b473ee516e15a7811a66
rwik19 <62844629+rwik19@users.noreply.github.com>
2020-04-07 13:43:16 +0530

    Add file

--- sine.c
+++ sine.c
@@ -1,29 +1,51 @@
 /*This program defines a sine function that uses the Taylor Series to calculate sines at very small angles.
 Using the Taylor series to calculate sine has certain limitations. However, one may utilise the periodicity
- of sin x to find sines of larger angles.*/
+ of sin x to find sines of larger angles.
+ It also compares the output with the sin function in the math.h library.*/
 
 #include<stdio.h>
+#include<math.h>
 
-double sine(double, int);
+double sine(double);
+double radian(double);
+double error(double,double);
 
 int main(void)
 {
-    float x;
-    int number_of_terms;
-    printf("x (in radians, preferably |x|< 2pi ~ 6) = ");
-    scanf("%f", &x);
-    printf("Number of terms till which the Taylor series will be calculated(preferably > 100) = ");
-    scanf("%d", &number_of_terms);
-    printf("sin(%0.3f) = %0.5f\n", x, sine(x, number_of_terms)); //output is formatted to 5 places after decimal.
+    double x;
+    for(int i=0;i<=90;i+=10)
+    {
+        if(i==0)
+        {
+            printf("Angle\t\tsin\t\tsine\t\t%%error\n");
+        }
+        x = radian((double)i);
+        //all values are printed correct upto 2 places of decimal.
+        printf("%d\t\t%0.2lf\t\t%0.2lf\t\t%0.2lf\n", i, sin(x), sine(x), error(sine(x), sin(x)));
+    }
 }
 
-double sine(double x, int n)
+double sine(double x)
 {
     double sum = x, term = x;
-    for(int i = 2; i<2*n; i+=2)
+    for(int i = 2; i<2*50; i+=2)
     {
         term *= -x*x/(i*(i+1));
         sum += term;
     }
     return sum;
+}
+
+double radian(double degree)
+{
+    return degree/180 * M_PI;
+}
+
+double error(double observed, double actual)
+{
+    if(actual == 0)
+    {
+        return 0;
+    }
+    return fabs(observed-actual)/actual *100;
 }
